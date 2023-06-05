2e87fc1a07757fd140155adac7562221fd2cf57c
berkaybyr <berkaybyr@icloud.com>
2023-06-05 03:18:42 +0300

    ui fixes

--- src/NewWorld.BiSMarket.Web/wwwroot/js/datatable.js
+++ src/NewWorld.BiSMarket.Web/wwwroot/js/datatable.js
@@ -3,16 +3,19 @@
     //add all elements a specific id and then use that id to get the table
     var tableIdList = [];
     var tableList = [];
+    var count = 0;
     tableElementList.each(function() {
         var tableId = $(this).attr("id");
+        console.log(this);
         //add id if null or undefined
         if (tableId == null || tableId == undefined) {
-            tableId = `table-${tableElementList.length}`;
+            tableId = `table-${count}`;
             $(this).attr("id", tableId);
         }
         tableIdList.push(tableId);
         const table = new DataTable(`#${tableId}`);
         tableList.push(table);
+        count++;
 
     });
 
